5cdf05728965dcd5230eb91a8b7340f01736a8a5
Aston .A Dahllof <137734906+Valleyleaf@users.noreply.github.com>
2024-08-16 17:07:35 -0300

    This is why you do your folder structure before getting too far into anything

--- client/src/assets/Header/Header.jsx
+++ client/src/assets/Header/Header.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./header.css"
+import Logo from '../img/BoDlogo.png'
+
 
 export default function Header(){
     return(
             <div>
                 <a href="/">
-                    <img className="logo" src="../public/Bodlogo.png"></img>
+                    <img className="logo" src={Logo}></img>
                 </a>
             </div>
     );
