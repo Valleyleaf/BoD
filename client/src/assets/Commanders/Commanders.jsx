1ca95a30bd13119abaf49caeac4e4bff588a3438
Aston .A Dahllof <137734906+Valleyleaf@users.noreply.github.com>
2024-10-18 19:21:22 -0300

    Reworking server structure to run off mongoDB+Mongoose. I just gotta re-learn how it works.

--- client/src/assets/Commanders/Commanders.jsx
+++ client/src/assets/Commanders/Commanders.jsx
@@ -1,10 +1,27 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useEffect, useState } from "react-router-dom";
 import {Characters} from '../Info/Characters'
 import "./commanders.css"
 // import displayUnit from '../../utils/unitAPI'
 
+useEffect(() => {
+    fetch('http://localhost:5000/api/commanders')
+      .then(response => response.json())
+      .then(data => setItems(data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
 function Commanders(){
+
+    const [commanders, setItems] = useState([]);
+
+    useEffect(() => {
+      fetch('http://localhost:5000/api/items')
+        .then(response => response.json())
+        .then(data => setItems(data))
+        .catch(error => console.error('Error fetching data:', error));
+    }, []);
+
     const renderItems = () => {
         return Characters.map((data, id) => (
             <div key={id} className='heroFrame comImg'>
--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,7 +1,14 @@
-const router = require('express').Router();
+const express = require('express');
+const commanders = require('../models/commanders');
+const router = express.Router();
 
-const unitRoutes = require('./unitRoutes');
-
-router.use('/commanders', unitRoutes);
+router.get('/commanders', async (req, res) => {
+  try {
+    const commanders = await Item.find();
+    res.json(commanders);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 
 module.exports = router;
