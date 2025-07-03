c49244748e0b4792f455c03a3e2bb74d416dadc6
Valleyleaf <aston_dahllof@outlook.com>
2025-07-03 11:25:31 -0300

    Adding a potential function to Home. Need to consider how to manage Characters.js. It will be thousands of lines long, need to break it down better.

--- client/src/assets/Home/Home.jsx
+++ client/src/assets/Home/Home.jsx
@@ -1,7 +1,13 @@
 import React, { useState } from "react";
 import GamePage from "../Game/Game.jsx"
+import { Characters } from "../Info/Characters.js";
 
 export default function Home() {
+
+    const rowCount = 3;
+    const eachRow = Math.ceil(Characters.length / rows);
+    const rows = Array.from({ length: rowCount }, (_, i) => Characters.slice(i * perRow, (i + 1) * perRow));
+
     return (
         <div className="flexColumn">
             <div>
@@ -14,8 +20,10 @@ export default function Home() {
                     </a>
                 </div>
                 <div>
-                    <h2>Insert hero carousel here. Copy from Commanders but use Character.Thumbnail, render 3 rows stacked ontop of each other</h2>
-                    <h2>Insert Highlight carousel here. What that is, I do not yet know. Screenshots?</h2>
+                    <Link to="/commanders" style={{ textDecoration: "none", color: "inherit" }}>    
+                        <h2>Insert hero carousel here. Copy from Commanders but use Character.Thumbnail, render 3 rows stacked ontop of each other</h2>
+                        <h2>Insert Highlight carousel here. What that is, I do not yet know. Screenshots?</h2>
+                    </Link>
                 </div>
             <div className="flexColumn center-content">
                     <h2>About:</h2>
