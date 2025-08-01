7546b167769f2584c16923165e6a690d434003b0
Aston Dahllöf <valleyleafsystems@gmail.com>
2025-08-01 17:20:55 -0300

    Messing around with stylings

--- client/src/assets/Header/Header.jsx
+++ client/src/assets/Header/Header.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Navbar from "../Navbar/Navbar.jsx";
 import "./header.css"
 import Logo from '../img/BoDlogo.png'
 
 
 export default function Header(){
     return(
-            <div>
+            <div className="flexRow center-content">
                 <a href="/">
                     <img className="logo" src={Logo}></img>
                     {/* Add background? Seems barren, needs more in terms of design. */}
                 </a>
+
             </div>
     );
 };
