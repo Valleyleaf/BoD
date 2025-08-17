d0cca04e9068555e02b633f487b041c8db93f35e
Aston Andersson Dahllof <aston_dahllof@outlook.com>
2025-08-17 11:46:59 -0300

    Sorting function working as intended. Might change how faction sorting works. Also working on HomeContent

--- client/src/assets/Commanders/Commanders.jsx
+++ client/src/assets/Commanders/Commanders.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import placeholder from '../img/placeholder.png';
-import Loading from '../Loading/Loading.jsx'
+import CommanderRenderDefault from './CommanderRenderDefault.jsx';
+import Loading from '../Loading/Loading.jsx';
 import "./commanderRender.css";
 import commanderService from '../../services/commanderService';
+import CommanderGroupedList from './CommanderGroupedByAttribute.jsx';
+import CommanderGroupedByFaction from './CommanderGroupedByFaction';
 
 function Commanders() {
   const [commanders, setCommanders] = useState([]);
@@ -45,18 +46,6 @@ function Commanders() {
     return items;
   };
 
-  const renderItems = () => {
-    return getSortedCommanders().map((data) => (
-      <Link to={`/commanders/${data.slug}`} key={data.name} className='heroFrame comImg'>
-        {data.image ? (
-        <img className='splashArt' src={data.image} alt={data.thumbnailAlt || data.name} loading="lazy" onError={(e) => { e.target.src = 'default-placeholder.png'; }}/>  
-        ):(
-          <img className='splashArt' src={placeholder} alt="Placeholder"/>
-        )}
-        <h2 className='hoverTitle'>{data.title}</h2>
-      </Link>
-    ));
-  };
 
   if (loading) return <Loading/>;
   if (error) return <div>Error: {error}</div>;
@@ -66,20 +55,25 @@ function Commanders() {
       <h2 className='Bio'>Your legend awaits.</h2>
         <div className="homeDivider"></div>
       <p>With over 60 Commanders and counting. Your playstyle is represented on the battlefield.</p>
-      {/* Dropdown menu for sorting */}
       <div className="commanderSortDropdownContainer" style={{ margin: "1em 0" }}>
         <label htmlFor="sortDropdown" style={{ marginRight: "0.5em" }}>Sort by:</label>
-        <select id="sortDropdown" value={sortOption} onChange={handleSortChange}>
-          <option value="Name">Name</option>
-          <option value="Primary Stat">Primary Stat</option>
+        <select className="dropDownMenu" id="sortDropdown" value={sortOption} onChange={handleSortChange}>
+          <option value="Name">Name(A-Z)</option>
+          <option value="Primary Stat">Keystone Attribute</option>
           <option value="Faction">Faction</option>
           <option value="Difficulty">Difficulty (1-5)</option>
         </select>
       </div>
       <div className="commanderRenderBackground fade-slide-up">
-        <div className='roster comList'>
-          {renderItems()}
-        </div>
+        {sortOption === "Primary Stat" ? (
+          <CommanderGroupedList commanders={getSortedCommanders()} />
+        ) : sortOption === "Faction" ? (
+          <CommanderGroupedByFaction commanders={getSortedCommanders()} />
+        ) : (
+          <div className='roster comList'>
+            <CommanderRenderDefault commanders={getSortedCommanders()} />
+          </div>
+        )}
       </div>
     </div>
 
--- client/src/assets/Game/GameRender.jsx
+++ client/src/assets/Game/GameRender.jsx
@@ -2,39 +2,30 @@ import React from "react";
 import topic from "./Topic.js";
 import "./game.css";
 
+function nextTopic(){
+
+}
+
+function previousTopic(){
+    
+}
 
 export default function GameRender() {
     return topic.map((data, i) => (
-        <div className="flexRow" key={i}>
-            {i % 2 === 0 ? (
-                <>
-                    {/* Render content Right */}
-                    <div className="topicContainer">
-                        <div className="flexColumn leftSide">
-                            <h2>{data.title}</h2>
-                            <img className="topicThumbnail" src={data.thumbnail} alt="" />
+        <div className="topicContainer">
+            <div className="topicInternal">
+                <div className="gameInfoBox">
+                    <h2>{data.title}</h2>
+                    <p>{data.content}</p>
+                        <div className="flexRow center-content">
+                            <button onClick={previousTopic}><h2>{data.buttonText}</h2></button>
+                            <button onClick={nextTopic}><h2>{data.buttonText}</h2></button>
                         </div>
-                        <div className="flexColumn rightSide center-content">
-                            <p>{data.content}</p>
-                        </div>
-                    </div>
-
-                </>
-            ) : (
-                <>
-                    {/* Render content Left */}
-                    <div className="topicContainer">
-                        <div className="flexColumn leftSide center-content">
-                            <p>{data.content}</p>
-                        </div>
-                        <div className="flexColumn rightSide">
-                            <h2>{data.title}</h2>
-                            <img className="topicThumbnail" src={data.thumbnail} alt="" />
-                        </div>
-                    </div>
-
-                </>
-            )}
+                </div>
+                <div className="gameImageBox">
+                    <img src={data.thumbnail} alt={data.alt} />
+                </div>
+            </div>
         </div>
     ));
 };
