67e23d528886edcbb7bf0fd9018e407a475e26de
Valleyleaf <aston_dahllof@outlook.com>
2025-11-07 16:43:31 -0400

    forgot useState

--- client/src/assets/LearnToPlay/learnObjectives/LearnObjectives.jsx
+++ client/src/assets/LearnToPlay/learnObjectives/LearnObjectives.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useState } from "react";
 import { useTranslation } from 'react-i18next';
 import objectivesJson from './objectivesJSON.json';
 import './learnobjectives.css';
 
 const LearnObjectives = () => {
   const { i18n } = useTranslation();
-
+  const [videoError, setVideoError] = useState(false);
   const languageData = objectivesJson[i18n.language] || objectivesJson.en;
   const { learnObjectives, objectives } = languageData;
+  const tempVideo = "https://res.cloudinary.com/dvutcekav/video/upload/v1758154917/Promo2_qvz6qi.mp4"
+  const handleVideoError = (e) => {
+      console.error('Video error:', e);
+      setVideoError(true);
+  };
 
   return (
     <div className="learn-objectives">
@@ -15,7 +20,6 @@ const LearnObjectives = () => {
         <h2>{learnObjectives.title}</h2>
         <p>{learnObjectives.description}</p>
       </div>
-
       <ul className="flexColumn center-content">
         {objectives.map((objective, index) => (
           <li key={index}>
@@ -31,7 +35,7 @@ const LearnObjectives = () => {
                         playsInline
                         preload="auto"
                         onError={handleVideoError}>
-                        <source src={objective.video} type="video/mp4" />
+                        <source src={tempVideo} type="video/mp4" />
                     </video>
                 ) : (
                     <div className="video-fallback">
