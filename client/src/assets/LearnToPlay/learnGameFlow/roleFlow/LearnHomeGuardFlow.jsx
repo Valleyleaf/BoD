5e5f0e9e1923f227b8e94d051c94a309781bbebd
Valleyleaf <aston_dahllof@outlook.com>
2026-03-20 15:52:54 -0300

    Adding additional JSON for other roles, still playing around with how to lay this page out

--- client/src/assets/LearnToPlay/learnGameFlow/roleFlow/LearnHomeGuardFlow.jsx
+++ client/src/assets/LearnToPlay/learnGameFlow/roleFlow/LearnHomeGuardFlow.jsx
@@ -1,20 +1,49 @@
 import './roleFlow.css';
 import { useTranslation } from 'react-i18next';
+import homeGuardFlowJSON from './homeGuardFlowJSON.json';
 
-const HomeGuardFlow = () => {
+const HomeGuardFlow = ({ onBack }) => {
     const { i18n } = useTranslation();
-    const [roleSelection, setRoleSelection] = useState(null);
     const languageData = homeGuardFlowJSON[i18n.language] || homeGuardFlowJSON.en;
     const { homeGuardFlow } = languageData;
-
-    const backClick = (roleId) => {
-    console.log('back click');
-    setRoleSelection(roleId);
-    };
-
+    
     return (
-    <div className='roleContainer'>
-        <h2>HomeGuard Flow</h2>
+    <div 
+    className='roleContainer'
+    style={{
+    backgroundImage: `linear-gradient(rgba(26, 26, 26, 0.73), rgba(3, 3, 3, 0.99)), url(${homeGuardFlow.backgroundImage})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center top',
+    backgroundRepeat: 'no-repeat'
+    }}
+    >
+        <div className='roleTitle'>
+            <button id='back' onClick={onBack}>Back</button>
+            <div>
+                <h2>Home Guard</h2>
+            </div>
+        </div>
+            <div className='roleDuty'>
+                <div>
+                    {homeGuardFlow.duties.map((duty, index) => (
+                        <div
+                        className='duty'
+                        style={{
+                        backgroundImage: `linear-gradient(rgba(26, 26, 26, 0.73), rgba(3, 3, 3, 0.99)), url(${duty.backgroundImage})`,
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center top',
+                        backgroundRepeat: 'no-repeat'
+                        }}
+                        key={index}>
+                            <div className='dutyTextContainer'>
+                                <h3>{duty.title}</h3>
+                                <p>{duty.description}</p>
+                            </div>
+                            <img src={duty.image} alt={duty.title} />
+                        </div>
+                    ))}
+                </div>       
+            </div>   
     </div>
     );
 
